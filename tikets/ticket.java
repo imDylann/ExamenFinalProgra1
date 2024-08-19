aba003cb393462e336b0cefa2db7548b8ff8740c
Dylan Quiros <dyquirosro@est.utn.ac.cr>
2024-08-19 13:44:02 -0600

    clase tiket

---
+++ tikets/ticket.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tikets;
+
+/**
+ *
+ * @author Student
+ */
+public class ticket {
+    
+   private String Tipo;
+   private int numero;
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public ticket(String Tipo, int numero) {
+        this.Tipo = Tipo;
+        this.numero = numero;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+   
+   
+    
+}
