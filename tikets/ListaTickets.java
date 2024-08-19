e38581dbd1f7cfe6a04853573cb3b1aa499c2a9d
ErikWarnke <erwarnkeva@est.utn.ac.cr>
2024-08-19 15:47:56 -0600

    ListaTickets
    clase lista tickets con metodo eliminarPos y addTicket

--- tikets/ListaTickets.java
+++ tikets/ListaTickets.java
@@ -4,10 +4,50 @@
  */
 package tikets;
 
-/**
- *
- * @author Student
- */
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 public class ListaTickets {
+    
+    private Queue<ticket> listaticketes;
+
+    private ListaTickets() {
+        this.listaticketes = new LinkedList<>();
+    }
+    
+    private static ListaTickets listatick;
+    
+     public static synchronized ListaTickets getInstance() {
+        if (listatick == null) {
+            listatick = new ListaTickets();
+        }
+        return listatick;
+    }
+        
+     public void addTicket(ticket tick){
+         this.listaticketes.add(tick);
+     }
+     
+     public void eliminarPos(int pos){
+                            
+
+        //List<ticket> list = new ArrayList<>(listatick); //aqui hay que pasar el queue a un list para buscar por valor!!!
+        
+        int positionToRemove = 2;  
+        if (positionToRemove >= 0 && positionToRemove < list.size()) {
+            list.remove(positionToRemove);
+        }
+        
+        listatick = new LinkedList<>(list);
+     }
+
+    @Override
+    public String toString() {
+        return "ListaTickets{" + "listaticketes=" + listaticketes + '}';
+    }
+    
     
 }
