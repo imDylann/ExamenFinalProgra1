bc27a6eeecb6994cd5cbc6c8ccf9e58c5c86504c
Dylan Quiros <dyquirosro@est.utn.ac.cr>
2024-08-19 16:32:06 -0600

    Correccion Controlador

--- tikets/Controlador.java
+++ tikets/Controlador.java
@@ -4,10 +4,34 @@
  */
 package tikets;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author Student
  */
 public class Controlador {
-    
+      private HashMap<String,Integer> ticketCounters;
+      private ListaTickets litaticketes;
+
+    public Controlador() {
+      
+        this.litaticketes = ListaTickets.getInstance();
+    }
+      
+       public void generar(String tipo) {
+          
+       if (!ticketCounters.containsKey(tipo)) {
+        ticketCounters.put(tipo, 0);
+    }
+        int numero = ticketCounters.get(tipo) + 1;
+        ticketCounters.put(tipo, numero);
+
+        ticket ticket = new ticket(tipo, numero);
+        litaticketes.addTicket(ticket);
+    }
+
+  
 }
